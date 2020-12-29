02257c2bc44290edc57445f41c007ff0988ec0b6
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2020-12-29 14:12:41 +0100

    add Javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+const ARRMOVIES=[
+    "the sinner",
+    "Venom",
+    "El Irlandes",
+    "Suits",
+    "La maldicion",
+    "Triple frontera",
+    "Defenders"
+]
+function clickSearchIcon (){
+    let icon = document.getElementById ("searchIcon");
+}
