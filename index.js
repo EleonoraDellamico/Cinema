0d4ab38a6256eabc9b6d37a0b72634bdd87d41ef
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2020-12-29 16:38:24 +0100

    Icon Hidden

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ const ARRMOVIES=[
     "Triple frontera",
     "Defenders"
 ]
+//PASOS A SEGUIR 
+//1 ON CLICK
+//2 AÑADIR EVENTLISTENER
+//3 PINTAR PELICULAS
+
+
 function clickSearchIcon (){
     let icon = document.getElementById ("searchIcon");
+    //1
+    icon.classList.toggle("selected")
+    let input = document.getElementById("searchInput");
+    let searchListContainer = document.getElementById ("searchListContainer");
+
+    
+    if(icon.classList.contains("selected")){
+
+        input.style.visibility = "visible";
+        input.style.opacity= 1;
+    }else{
+         input.style.visibility = "hidden";
+         input.style.opacity= 0;
+
+    }
 }
